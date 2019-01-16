c1f64cd1ca1fc46b9e4ea639e564a23fdc153908
pskwiercz <przemek.skwiercz@gmail.com>
2019-01-16 22:46:22 +0100

    Add POJOs

---
+++ src/main/java/com/pskwiercz/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.pskwiercz.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/pskwiercz/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.pskwiercz.springpetclinic.model;
+
+public class Vet extends Person {
+}
