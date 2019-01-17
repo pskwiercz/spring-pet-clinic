5e49230dc86a4f0f9eea30bfba0afff19d5918a1
pskwiercz <przemek.skwiercz@gmail.com>
2019-01-17 23:03:48 +0100

    Implemet interfaces for services

---
+++ pet-clinic-data/src/main/java/com/pskwiercz/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.pskwiercz.springpetclinic.services;
+
+import com.pskwiercz.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
