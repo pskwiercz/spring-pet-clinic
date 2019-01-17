b1c61c726485f12ccdc8d7397abe838357c1c6f8
pskwiercz <przemek.skwiercz@gmail.com>
2019-01-17 23:11:25 +0100

    Add BaseEntity

--- pet-clinic-data/src/main/java/com/pskwiercz/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/pskwiercz/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.pskwiercz.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/pskwiercz/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/pskwiercz/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.pskwiercz.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
